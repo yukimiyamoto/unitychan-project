54660b78a0a9d485185e765cf3249b7bf63173ba
yuki_miyamoto <yuki.miyamoto@playnext.co.jp>
2016-01-23 00:08:58 +0900

    AI仮完成

---
+++ Unitychan/Assets/Scripts/LoiteringErea.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoiteringErea : MonoBehaviour 
+{
+
+	public float warkspeed;
+
+	void OnTriggerStay(Collider zombie)
+	{
+		//ahead
+		zombie.transform.Translate (Vector3.forward * warkspeed * Time.deltaTime);
+	}
+
+	void OnTriggerExit(Collider enemy)
+	{
+		if (enemy.gameObject.tag == "zombie") 
+		{
+			enemy.transform.Rotate(0 , enemy.transform.rotation.y + (100 * Random.Range(-0.8f,-1.0f)) , 0);
+		}
+	}
+}
