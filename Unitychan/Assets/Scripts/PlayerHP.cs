55af714c2aa7d264ac95684fa33e7b524e85365a
yuki_miyamoto <yuki.miyamoto@playnext.co.jp>
2016-01-22 17:53:33 +0900

    HP表示

--- Unitychan/Assets/Scripts/PlayerHP.cs
+++ Unitychan/Assets/Scripts/PlayerHP.cs
@@ -1,15 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class PlayerHP : MonoBehaviour {
+public class PlayerHP : MonoBehaviour 
+{
+	//PlayerHP
+	public GameObject[] PlayerLife;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
-	void Update () {
-	
+	public void UpdateLife (int life) 
+	{
+		//DisplayHP
+		for (int lifeindex = 0; lifeindex<PlayerLife.Length; lifeindex++) 
+		{
+			if(lifeindex<life)
+			{
+				PlayerLife[lifeindex].SetActive(true);
+			}
+			else
+			{
+				PlayerLife[lifeindex].SetActive(false);
+			}
+		}
 	}
 }
